a21693a4926957ec1df5193c2912288097c06f07
Ndumiso <zamokuhlen76@gmail.com>
2023-03-17 09:19:00 -0700

    Generating random numbers and checking if they're <>= 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Description: 'The program generates random numbers'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* Check whether n is positive/negative/zero */
+	if (n > 0)
+	{	printf("%i is positive\n");
+	}
+	else if (n < 0)
+	{	printf("%i is negative\n");
+	}
+	else if (n == 0)
+	{	printf("%i is zero");
+	}
+	return (0);
+}
