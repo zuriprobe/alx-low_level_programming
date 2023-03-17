236e1a94fc9064d800d39b36fb6208357ada43b2
Ndumiso <zamokuhlen76@gmail.com>
2023-03-17 15:42:03 -0700

    Printing base 10 numbers from 0 to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * Description: 'The program generates numbers from 0 to 10'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+	return (0);
+}
+		
