f7cfa4150c532be487e4a5950711628e5ad9cb74
Ndumiso <zamokuhlen76@gmail.com>
2023-03-17 13:20:46 -0700

    Printing letters in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * Description: 'The program generates letters from A to Z'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	char ch;
+	for(ch = 'A' ; ch <= 'Z' ; ch++)
+	{	putchar(tolower(ch));
+	}
+	putchar('\n');
+	return(0);
+}
