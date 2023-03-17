5f3ed0702c5e972deacf0978d27451463ff0910e
Ndumiso <zamokuhlen76@gmail.com>
2023-03-17 04:47:33 -0700

    Print the exact string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/*Write a C program that prints a string*/
+
+/*Write a C program that prints a string
+ * Use the function puts */
+
 int main(void)
 {	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
