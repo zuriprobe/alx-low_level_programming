dd60280956ea63b4407bd0467f2ed8379e2076b7
Ndumiso <zamokuhlen76@gmail.com>
2023-03-17 15:06:09 -0700

    Omitting e and q from the sequence

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * Description: 'The program generates letters from A to Z'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		if (tolower(ch) == 'e' || tolower(ch) == 'q')
+		{
+			continue;
+		}
+		putchar(tolower(ch));
+	}
+	putchar('\n');
+	return (0);
+}
