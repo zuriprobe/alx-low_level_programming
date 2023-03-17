f9324cb8b5fe15a8d0a353faf6e0a98c4f9fdf64
Ndumiso <zamokuhlen76@gmail.com>
2023-03-17 16:26:16 -0700

    Printing output of A to Z in reverse using lowercase

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * Description: 'The program generates letters from Z to A'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{	char ch;
+	for(ch = 'Z' ; ch >= 'A' ; ch--)
+	{	putchar(tolower(ch));
+	}
+	putchar('\n');
+	return (0);
+}
