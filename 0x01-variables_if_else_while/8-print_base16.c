0b07e3e472a3811eb8f62ff89d75f07df1490d22
Ndumiso <zamokuhlen76@gmail.com>
2023-03-17 18:30:04 -0700

    q

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * Description: 'The program generates letters from A to Z'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{	for (int i = 0; i < 16; i++)
+	{	printf("%x", i);
+	}
+	return (0);
+}
