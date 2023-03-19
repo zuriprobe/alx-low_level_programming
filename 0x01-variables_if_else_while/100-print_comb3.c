2e05235185b26c8ec8d94d3b8b307ed33dc3f99f
Ndumiso <zamokuhlen76@gmail.com>
2023-03-19 11:42:18 -0700

    Generating ascending two digit numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * Description: 'The program generates two digit numbers using putchar'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	int n  = ',';
+	int m = 10;
+	int counter = 2;
+
+	while (i <= 90)
+	{
+		if (i == m)
+		{	i += counter;
+			counter++;
+			m += 10;
+		}
+		else if (i <= 90)
+		{	putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			if (i < 89)
+			{	putchar( n );
+				putchar(' ');
+			}
+			i++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
