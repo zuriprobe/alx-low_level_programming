0389e56578430acc2db29de6c7493ef230eb2a1f
Ndumiso <zamokuhlen76@gmail.com>
2023-03-22 16:32:56 -0700

    Check if n is greater/smaller/equal to zero

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,24 +3,27 @@
 /**
  * print_sign - Entry point
  *
- * Description: 'The function checks if n is greater/less/equal to 0'
+ * Description: 'The function checks if n is greater or less or equal to 0'
  * @n: is an integer variable
  *
- * Return 1/0/-1
+ * Return 1 or 0 or -1
  */
 
 int print_sign(int n)
-{
+{	
 	if (n > 0)
-	{	_putchar('+');
+	{
+		_putchar('+');
 		return (1);
 	}
-	else if (n == 0)
-	{	_putchar('0');
-		return (0);
-	}
 	else if (n < 0)
-	{	_putchar('-');
+	{
+		_putchar('-');
 		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
 	}
 }
