e9fc71e92bda9a37d87a969b05c9365b103f665c
Ndumiso <zamokuhlen76@gmail.com>
2023-03-22 06:20:13 -0700

    Creating a function that prints a - z with no return values

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - is a prototype function that prints letters from a to z
  *
  * Return: Always 0.
  */
@@ -9,6 +9,7 @@
 void print_alphabet(void)
 {
 	char i = 'a';
+
 	while (i <= 'z')
 	{
 		_putchar(i);
