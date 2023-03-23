ddddfbcbd06c3e3a6ac9b1d2d546eb9d8e68f2c1
Ndumiso <zamokuhlen76@gmail.com>
2023-03-23 13:48:46 -0700

    Multiplying two inputs

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - Entry point
+ *
+ * Description: 'Multiplies the two parameters'
+ * @a and @b: Integer variables
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
