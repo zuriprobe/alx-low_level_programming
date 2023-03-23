36864e7dc125643bc78be7ed53481731df8b7cf1
Ndumiso <zamokuhlen76@gmail.com>
2023-03-23 13:18:03 -0700

    Checking if the input is upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isupper - Entry point
+ *
+ * Description: 'Checks if the input is upper case'
+ * @c: Integer variable
+ *
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
