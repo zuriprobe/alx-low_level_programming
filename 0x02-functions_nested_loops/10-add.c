56a1f9f1738a91015f47ece69b9432797818241e
Ndumiso <zamokuhlen76@gmail.com>
2023-03-22 20:20:29 -0700

    Adding two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib>
+
+/**
+ * add - Entry point
+ *
+ * Description: 'Addition of two numbers'
+ *@a and b: Integer variables
+
+ * Return: Always 0.
+ */
+
+int add(int a, int b)
+{
+	int n;
+	n = a + b;
+	return (n);
+}
