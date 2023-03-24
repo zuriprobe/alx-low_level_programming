3aeea63f6b1c4948c3cb25ad40edb3934619c568
Ndumiso <zamokuhlen76@gmail.com>
2023-03-24 13:38:39 -0700

    Printing a diagonal using '\'

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal - Entry point
+ *
+ * Description: 'Printing a diagonal of slashes'
+ * @n: Integer variable
+ *
+ * Return void
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int c;
+	
+	for (i = 0; i < n; i++)
+	{
+		for (c = 0; c < i; c++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,3 +5,4 @@ void print_numbers(void);
 int _putchar(char c);
 void more_numbers(void);
 void print_line(int n);
+void print_diagonal(int n);
