8a174e9992008214927f054dfeb238c5883392a3
Ndumiso <zamokuhlen76@gmail.com>
2023-03-24 17:02:55 -0700

    Replacing multiplys of 3 and 5

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Printing multiplies of 3 and 5'
+ *
+ * Return: void
+ */
+
+int main()
+{
+	int i;
+
+	char multiple_of_3[] = "Fizz";
+	char multiple_of_5[] = "Buzz";
+	char multiple_of_both[] = "FizzBuzz";
+
+	i = 1;
+
+	while (i < 100)
+	{
+		if ((i % 3) != 0)
+		{
+			printf("%i", i);
+			putchar(' ');
+			i++;
+		}
+		if (((i % 3) == 0) && ((i % 5) == 0))
+		{
+			printf("%s", multiple_of_both);
+			putchar(' ');
+			i++;
+		}
+		if ((i % 3) == 0)
+		{
+			printf("%s", multiple_of_3);
+			putchar(' ');
+			i++;
+		}
+		if ((i % 5) == 0)
+		{
+			printf("%s", multiple_of_5);
+			putchar(' ');
+			i++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
