a739dd4b98c3c2566c41cb9c58f0f28e2a2dfde7
Ndumiso <zamokuhlen76@gmail.com>
2023-03-24 19:31:06 -0700

    Hot fix

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -14,13 +14,13 @@ void print_line(int n)
 {
 	int i;
 
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
 	for (i = 0; i < n; i++)
 	{
 		_putchar('-');
+	}
+	if (n <= 0)
+	{
+		_putchar('\n');
 	}
 	_putchar('\n');
 }
