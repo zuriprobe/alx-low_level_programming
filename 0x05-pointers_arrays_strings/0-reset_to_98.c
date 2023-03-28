97446092d0b7c60183436a7140dcb074b828dca7
Ndumiso <zamokuhlen76@gmail.com>
2023-03-27 17:54:23 -0700

    Changing the value of n using the address of the variable

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ *
+ * Description: 'change the value of main function'
+ * @n: Integer variable
+ *
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
