e244006f1551e852434a4be4820020349b2b7f9f
Ndumiso <zamokuhlen76@gmail.com>
2023-03-31 16:36:14 -0700

    Using leet code

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - Entry point
+ * @str: variable
+ * Return: str
+ */
+
+char *leet(char *str)
+{
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == s2[i])
+			{
+				str[i] == s2[j];
+			}
+		}
+	}
+	return (n)
+}
