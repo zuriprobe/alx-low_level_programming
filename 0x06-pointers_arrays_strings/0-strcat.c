7c00070896df037c410b61c47f6edbbbd22f26f8
Ndumiso <zamokuhlen76@gmail.com>
2023-03-31 06:25:32 -0700

    Merging two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,18 +15,19 @@ char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
-	int length;
 
-	length = strlen(dest);
-
-	for (i = 0; dest[i] != '\0'; i++)
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		for (j = 0; src[j] != '\0'; j++)
-		{
-			dest[length] = src[i];
-			i++;
-		}
+		i++;
 	}
-	dest[length] = '\0';
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
