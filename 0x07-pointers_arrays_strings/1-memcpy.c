99f34ffe27194339747d55c9e5a3e07815ad9560
Ndumiso <zamokuhlen76@gmail.com>
2023-04-04 19:12:44 -0700

    Copying memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdion.h>
+
+/**
+ * _memcpy - Entry point
+ * @dest: pointer
+ * @src: pointer
+ * @n: integer variable
+ * Return: pointer
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
