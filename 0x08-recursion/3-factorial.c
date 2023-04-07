711646a24797e22a0ca228022cf8ddb5330765c8
Ndumiso <zamokuhlen76@gmail.com>
2023-04-06 19:49:09 -0700

    Printing factorials

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * factorial - Entry point
+ * @n: int input
+ * Return: int
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
