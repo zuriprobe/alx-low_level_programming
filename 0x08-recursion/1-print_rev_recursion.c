3631836f0802978255ba99da88ba9750d8444f46
Ndumiso <zamokuhlen76@gmail.com>
2023-04-06 18:38:21 -0700

    Printing the string in revers

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,12 +9,13 @@
 
 void _print_rev_recursion(char *s)
 {
-	while (*s != '\0')
+	if (*s == '\0')
 	{
-		s++;
+		return;
 	}
-	for (; *s != '\n'; s--)
+	if (*s)
 	{
-		putchar(*s);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 }
