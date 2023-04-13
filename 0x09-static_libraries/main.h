6d59a85d645e89a2ba300ca0c32f62551bb63c62
Ndumiso <zamokuhlen76@gmail.com>
2023-04-13 15:54:01 -0700

    Creating a static library

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -16,3 +17,4 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+#endif
