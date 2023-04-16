578561e40a28ab82e1ca66b95276366819ce180f
Ndumiso <zamokuhlen76@gmail.com>
2023-04-15 17:22:48 -0700

    Printing the name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: number of arguments passed by cmd
+ * @argv: array of of string arguments
+ * Return: 0
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	_putchar('\n');
+	return (0);
+}
